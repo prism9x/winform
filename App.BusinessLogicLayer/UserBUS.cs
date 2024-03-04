156d6ac2558f6105ff2c02443fc7ae018cc36ab9
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-03-04 23:41:01 +0700

    Add function GetAll

--- App.BusinessLogicLayer/UserBUS.cs
+++ App.BusinessLogicLayer/UserBUS.cs
@@ -1,5 +1,6 @@
 ﻿using App.DataAccessLayer;
 using App.DataTransferObject;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace App.BusinessLogicLayer
@@ -8,6 +9,15 @@ namespace App.BusinessLogicLayer
     {
         public DbConnection db = new DbConnection();
         public User user = new User();
+
+        public DataTable GetAll()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM tb_users";
+
+            return db.ExeReader(cmd);
+        }
         public int Insert(User user)
         {
             // Tạo câu lệnh SQL
@@ -26,7 +36,7 @@ namespace App.BusinessLogicLayer
             //    VALUES(@Username, @Password, @Role);
             //END;
 
-            SqlCommand cmd = new SqlCommand("InsertUser", db.GetCon());
+            SqlCommand cmd = new SqlCommand("InsertUser");
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@Username", user.Name);
             cmd.Parameters.AddWithValue("@Password", user.Password);
@@ -35,5 +45,26 @@ namespace App.BusinessLogicLayer
             // Thực thi câu lệnh SQL
             return db.ExeNonQuery(cmd);
         }
+
+        /// <summary>
+        /// Hàm update info truyền vào một đối tượng
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public int Update(User user)
+        {
+            SqlCommand cmd = new SqlCommand("UpdateUserInfoById", db.GetCon());
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@UserId", user.Id);
+            cmd.Parameters.AddWithValue("@NewUsername", user.Name);
+            cmd.Parameters.AddWithValue("@NewPassword", user.Password);
+            cmd.Parameters.AddWithValue("@NewRoleId", user.RoleId);
+
+
+            // Thực thi câu lệnh SQL
+            return db.ExeNonQuery(cmd);
+        }
+
+
     }
 }
